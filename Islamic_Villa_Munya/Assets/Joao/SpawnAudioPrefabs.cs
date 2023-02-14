c4a7c497d1940c744da74f0b54b18e75079c4a08
JOÃO CRESPO GONÇALVES <2208166@uad.ac.uk>
2023-02-14 16:27:38 +0000

    Joao New AudioManager
    Joao: a updated version of the audio Manager, had to replace a lot. 3D, 2D and a destroy script.

--- Islamic_Villa_Munya/Assets/Joao/SpawnAudioPrefabs.cs
+++ Islamic_Villa_Munya/Assets/Joao/SpawnAudioPrefabs.cs
@@ -6,21 +6,17 @@ public class SpawnAudioPrefabs : MonoBehaviour
 {
     [SerializeField] private Transform[] spawnLocations;
     [SerializeField] private GameObject[] WhatToSpawn;
-    //[SerializeField] private GameObject[] WhatToSpawnClone; // instantiated objects
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     public void spawnAudioPrefab(int Num, bool attachTo = false)
     {
         WhatToSpawn[Num] = Instantiate(WhatToSpawn[Num], spawnLocations[Num].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        // spawns prefabs number
 
         if (attachTo)
         {
             WhatToSpawn[Num].transform.parent = spawnLocations[Num].transform;
+            // If TRUE, spawns object as child of spawn location
         }
     }
 }
---
+++ Islamic_Villa_Munya/Assets/Joao/_TestingAudio.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _TestingAudio : MonoBehaviour
+{
+    [SerializeField] SpawnAudioPrefabs SpawnAudio;
+    // Get AudioManager for audio spawning
+
+    
+    void Start()
+    {
+        SpawnAudio.spawnAudioPrefab(0, true);  //Spawn audio prefab (prefab is 3D in this case) as a child of spawn location BECAUSE of TRUE
+        SpawnAudio.spawnAudioPrefab(1);        //Spawn audio prefab (prefab is 2D in this case) and NO TRUE cause it is a 2D sound (ambient sound)
+
+    }
+
+}
