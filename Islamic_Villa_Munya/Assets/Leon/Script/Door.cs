cc06921fde120d7ae870f62dab650297305dea8b
DX9 <leonhodgkinson123@gmail.com>
2023-02-27 19:14:25 +0000

    Doors finally work, locking, door puppeting

--- Islamic_Villa_Munya/Assets/Leon/Script/Door.cs
+++ Islamic_Villa_Munya/Assets/Leon/Script/Door.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,42 +5,41 @@ using UnityEngine.Events;
 [ExecuteInEditMode]
 public class Door : MonoBehaviour
 {
-    public float originAngle = 90.0f;
-    [Range(0.1f, 20)]
-    public float hingeOffset = 1f;
-    [Range(-179, 179)]
+    [Header("Door Parameters")]
+    [Range(0.1f, 10)]
+    public float hingeDistanceOffset = 1f;
+    [Range(-180, 180)]
+    public float angleOffset = 90.0f;
+    [Range(-180, 180)]
     public float angleOpen = 90.0f;
 
-    public Vector3 orientation = Vector3.right;
-    
-    float offsetRot = 0f;
     public float openSpeed = 1f;
     public float closeSpeed = 1f;
+
     public bool open = false;
-    //public bool destinationReached = false;
-    //door holder is an empty object that serves as the hinge which this gameobject gets parented to
-    GameObject doorHolder;
+    public bool locked = false;
+
+    Vector3 doorToHingeVector = Vector3.forward;
+    GameObject hinge, hingeOpenTarget, hingeClosedTarget;
     Vector3 hingePos;
     Vector3 openPos;
     Vector3 closedPos;
 
-    //public UnityEvent doorOpen;
+    Mesh mesh;
+    [Header("Door Extras")]
+    public Door puppetDoor;
+    public UnityEvent doorReachOpen, doorReachClose;
+
 
     void Start()
     {
-        //on game start, make a hinge for this door
-        if(Application.isPlaying)
-            CreateDoorParent();
         //store the open and close positions
         UpdatePositions();
-    }
-
-    void UpdatePositions()
-    {
-        //update the position of the hinge and open position in real time in edit mode
-        hingePos = transform.position + orientation * hingeOffset;
-        closedPos = hingePos + Quaternion.Euler(0, 0, 0) * orientation * -Vector3.Distance(hingePos, transform.position);
-        openPos = hingePos + Quaternion.Euler(0, angleOpen + offsetRot, 0) * orientation * -Vector3.Distance(hingePos, transform.position);
+        //on game start, make a hinge for this door object
+        if (Application.isPlaying)
+        {
+            CreateDoorParent();
+        }
     }
 
     void Update()
@@ -51,33 +48,65 @@ public class Door : MonoBehaviour
         if (!Application.isPlaying)
         {
             UpdatePositions();
+            mesh = gameObject.GetComponent<MeshFilter>().sharedMesh;
             //stuff breaks if door not kept vertical so z and x are locked for now
             Vector3 rot = transform.eulerAngles;
             rot.x = rot.z = 0;
-            transform.rotation = Quaternion.Euler(rot.x, rot.y, rot.z); 
+            transform.rotation = Quaternion.Euler(rot.x, rot.y, rot.z);
         }
-        //update the door position if not at destination
-        else if (doorHolder != null)// && !destinationReached)
+        else if (hinge != null)
+        {
+            if (Input.GetKeyDown(KeyCode.F))//TEMPORARY
+            {
+                ToggleOpen();
+            }
+
+            if (locked)
+                open = false;
+
             MoveDoor(open);
+
+            if (puppetDoor != null)
+            {
+                puppetDoor.open = open;
+                puppetDoor.locked = locked;
+            }
+        }// && !destinationReached)
+    }
+    void UpdatePositions()
+    {
+        doorToHingeVector = AddAngleToVector(angleOffset, transform.forward);
+        closedPos = transform.position;
+        hingePos = closedPos + doorToHingeVector * hingeDistanceOffset;
+        openPos = hingePos - AddAngleToVector(angleOpen, doorToHingeVector) * hingeDistanceOffset;
+    }
+
+    Vector3 AddAngleToVector(float angle, Vector3 vector)
+    {
+        return Quaternion.Euler(0, angle, 0) * vector;
     }
 
     //move the door to its open or not open position
     void MoveDoor(bool open = true)
     {
-        //set appropriate values based on open state
-        Vector3 targetDirection = (open? openPos : closedPos) - doorHolder.transform.position;
-        float speed = open ? openSpeed : closeSpeed;
+        //set appropriate values based on if door is to be open or closed
+        Vector3 targetDirection = open ? hingeOpenTarget.transform.forward : hingeClosedTarget.transform.forward;
 
-        // The step size is equal to speed times frame time.
+        float speed = open ? openSpeed : closeSpeed;
+        // step size is equal to speed times frame time.
         float singleStep = speed * Time.deltaTime;
-        // Rotate the forward vector towards the target direction by one step
-        Vector3 newDirection = Vector3.RotateTowards(doorHolder.transform.forward, targetDirection, singleStep, 0.0f);
+        // rotate forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(hinge.transform.forward, targetDirection, singleStep, 0.0f);
         // Calculate a rotation a step closer to the target and applies rotation to this object
-        doorHolder.transform.rotation = Quaternion.LookRotation(newDirection);
+        hinge.transform.rotation = Quaternion.LookRotation(newDirection);
 
         //stop moving the door if at destination angle
         //if(CheckAngleReached(doorHolder.transform.forward, targetDirection))        
-            //destinationReached = true;
+        //destinationReached = true;
+
+        //hinge.transform.rotation = Quaternion.LookRotation(hingePos - openPos) * Quaternion.Euler(0, -angleOffset, 0);
+        //if(open)
+        //  hinge.transform.rotation = null
     }
 
     //return true when angle between vectors in near zero
@@ -89,33 +118,61 @@ public class Door : MonoBehaviour
     //spawns an empty gameobject to serve as the door hinge point and parents this object (door mesh) to it
     void CreateDoorParent()
     {
-        doorHolder = new GameObject("Door Holder");
-        doorHolder.transform.parent = null;
-        hingePos = transform.position + orientation * hingeOffset;
-        //doorHolder.transform.rotation = transform.rotation * Quaternion.Euler(0, -90, 0);
-        doorHolder.transform.position = hingePos;
-        transform.parent = doorHolder.transform;
+        hinge = new GameObject(gameObject.name + " Hinge"); // The actual hinge
+        hingeClosedTarget = new GameObject(gameObject.name + " Hinge Target Closed"); // reference point for closed position
+        hinge.transform.position = hingeClosedTarget.transform.position = hingePos;
+        //hinge should face door for easier rotations
+        hinge.transform.rotation = hingeClosedTarget.transform.rotation = Quaternion.LookRotation(closedPos - hinge.transform.position);
+        hingeClosedTarget.hideFlags = HideFlags.HideInHierarchy;
+
+        //open position
+        hingeOpenTarget = new GameObject(gameObject.name + " Hinge Target Open");
+        hingeOpenTarget.transform.position = hingePos;
+        hingeOpenTarget.transform.rotation = Quaternion.LookRotation(openPos - hinge.transform.position);
+        hingeOpenTarget.hideFlags = HideFlags.HideInHierarchy;
+
+        transform.parent = hinge.transform;
     }
 
     //editor helpers
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
+        //Hinge Pos
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(hingePos, 0.2f);
         Handles.Label(hingePos + Vector3.up * 0.3f, "Hinge");
+        //Open Pos
         Gizmos.color = Color.green;
         Gizmos.DrawLine(hingePos, openPos);
         Gizmos.DrawSphere(openPos, 0.05f);
         Handles.Label(openPos + Vector3.up * 0.2f, "Open");
+        //Closed Pos
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(transform.position, hingePos);
-        Gizmos.DrawSphere(transform.position, 0.05f);
-        Handles.Label(transform.position + Vector3.up * 0.2f, "Closed");
+        Gizmos.DrawLine(closedPos, hingePos);
+        Gizmos.DrawSphere(closedPos, 0.05f);
+        Handles.Label(closedPos + Vector3.up * 0.2f, "Closed");
+        //Open Preview
+        Color c = new Color(0, 100, 200, 0.5f);
+        Gizmos.color = c;
+        Gizmos.DrawWireMesh(mesh, openPos, Quaternion.LookRotation(hingePos - openPos) * Quaternion.Euler(0, -angleOffset, 0), transform.localScale);
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Open Preview
+        Color c = new Color(0, 100, 200, 0.3f);
+        Gizmos.color = c;
+        Gizmos.DrawMesh(mesh, openPos, Quaternion.LookRotation(hingePos - openPos) * Quaternion.Euler(0, -angleOffset, 0), transform.localScale);
     }
 
     public bool ToggleOpen()
     {
         open = !open;
         return open;
+    }
+    public bool ToggleLocked()
+    {
+        locked = !locked;
+        return locked;
     }
 }
