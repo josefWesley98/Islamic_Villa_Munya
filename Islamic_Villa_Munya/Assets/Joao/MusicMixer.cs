eb73887bc8ec4e25ed9111add7194186e3a951e8
JOÃO CRESPO GONÇALVES <2208166@uad.ac.uk>
2023-03-01 11:49:38 +0000

    Joao_Sounds placed and tested

--- Islamic_Villa_Munya/Assets/Joao/MusicMixer.cs
+++ Islamic_Villa_Munya/Assets/Joao/MusicMixer.cs
@@ -7,23 +7,25 @@ public class MusicMixer : MonoBehaviour
 {
     public AudioMixerSnapshot Area1;
     public AudioMixerSnapshot Area2;
-    public AudioMixerSnapshot Area3;
+    //public AudioMixerSnapshot Area3;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Area1"))
         {
+            Debug.Log("Area1");
             Area1.TransitionTo(5);
         }
 
         if (other.CompareTag("Area2"))
         {
+            Debug.Log("Area2");
             Area2.TransitionTo(2);
         }
 
-        if (other.CompareTag("Area3"))
+        /*if (other.CompareTag("Area3"))
         {
             Area3.TransitionTo(5);
-        }
+        }*/
     }
 }
