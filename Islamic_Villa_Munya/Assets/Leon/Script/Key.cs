31a949b9b5f086d7ab56a736b68dadfdf34795ed
DX9 <leonhodgkinson123@gmail.com>
2023-03-07 22:54:09 +0000

    added a little spin and bob to the key

--- Islamic_Villa_Munya/Assets/Leon/Script/Key.cs
+++ Islamic_Villa_Munya/Assets/Leon/Script/Key.cs
@@ -3,11 +3,23 @@ using UnityEngine;
 public class Key : MonoBehaviour
 {
     public Door doorToUnlock;
+    float startY;
+
+    private void Start()
+    {
+        startY = transform.position.y;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         GameManager.SetHasKey(true);
         doorToUnlock.unlockNextPress = true;
         Destroy(gameObject, 0.3f);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, 60 * Time.deltaTime, Space.World);
+        transform.position = new Vector3(transform.position.x, startY + Mathf.Sin(Time.time * 1.5f) * 0.15f, transform.position.z);
     }
 }
