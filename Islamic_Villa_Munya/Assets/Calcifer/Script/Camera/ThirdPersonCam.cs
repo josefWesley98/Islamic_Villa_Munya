1d70179dd06e2c341e0e100b4ccf75967530daa7
UAD\1903400 <1903400@uad.ac.uk>
2023-03-14 13:26:58 +0000

    New Player Prefab
    Movement sorted. Climbing ongoing.

--- Islamic_Villa_Munya/Assets/Calcifer/Script/Camera/ThirdPersonCam.cs
+++ Islamic_Villa_Munya/Assets/Calcifer/Script/Camera/ThirdPersonCam.cs
@@ -26,17 +26,19 @@ public class ThirdPersonCam : MonoBehaviour
         Vector3 view_dir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = view_dir.normalized;
 
-        //Rotate the player
-        if(p.GetIsClimbing())
-        {
-            ground_rot_speed = 0;
-        }
-        else
-        {
-            ground_rot_speed = 15;
-        }
+        ////Rotate the player
+        //if(p.GetIsClimbing())
+        //{
+        //    ground_rot_speed = 0;
+        //}
+        //else
+        //{
+        //    ground_rot_speed = 15;
+        //}
 
-            Vector2 look_at = p.GetPlayerInput();
+        ground_rot_speed = 15;
+
+        Vector2 look_at = p.GetPlayerInput();
             Vector3 input_dir = orientation.forward * look_at.y + orientation.right * look_at.x;
 
             player_obj.forward = Vector3.Slerp(player_obj.forward, input_dir.normalized, Time.deltaTime * ground_rot_speed);
