a0275e0c448cff45109ee259a8358a45eadec292
DX9 <leonhodgkinson123@gmail.com>
2023-03-19 23:58:45 +0000

    playeraudio script, more bricks stuff

---
+++ Islamic_Villa_Munya/Assets/Leon/Script/PlayerAudio.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip[] insideFootsteps;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
