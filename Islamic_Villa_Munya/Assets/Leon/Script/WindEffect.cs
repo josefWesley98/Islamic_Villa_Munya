612270ed32121fa001165f704c545e1d3b4fca3a
DX <leonhodgkinson123@gmail.com>
2023-04-26 13:46:34 +0100

    Wind particle and hanging light physics

---
+++ Islamic_Villa_Munya/Assets/Leon/Script/WindEffect.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindEffect : MonoBehaviour
+{
+   public float forceTimeMin = 0.1f;
+   public float forceTimeMax = 0.5f;
+
+    public float forceAmountMin = 10f;
+
+    public float forceAmountMax = 30f;
+
+    float timer = 0.0f;
+
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            rb.AddForce(Vector3.back * Random.Range(forceAmountMin, forceAmountMax));
+
+            ResetRandomTimer();
+        }
+    }
+
+    void ResetRandomTimer()
+    {
+        timer = Random.Range(forceTimeMin, forceTimeMax);
+    }
+}
