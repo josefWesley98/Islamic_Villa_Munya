e6dbcbad5b2197ed5d41a70b72a1405715251175
Calc1fer <1903400@uad.ac.uk>
2023-04-29 14:42:13 +0100

    Player Initiates Artefact
    The player has to interact with the pedestal to put the artefact there when it is collected. Woop!

--- Islamic_Villa_Munya/Assets/Calcifer/Script/Objects/PlaceArtefact.cs
+++ Islamic_Villa_Munya/Assets/Calcifer/Script/Objects/PlaceArtefact.cs
@@ -10,7 +10,9 @@ public class PlaceArtefact : MonoBehaviour
     [SerializeField] private GameObject artefact;
     [SerializeField] private GameObject empty_pedestal;
     [SerializeField] private int artefact_id;
-    private bool replace_artefact = false;
+    private bool replace_artefact;
+    private bool can_place = false;
+    private bool do_until = true;
 
     // Start is called before the first frame update
     void Start()
@@ -22,17 +24,26 @@ public class PlaceArtefact : MonoBehaviour
     private void Update() 
     {
         replace_artefact = kb.fKey.isPressed;
+
+        if(do_until)
+        {
+            if(replace_artefact && can_place)
+            {
+                artefact.SetActive(true);
+                Destroy(empty_pedestal);
+
+                do_until = false;
+            }
+        }
     }
 
     //If the player has collected relevant artefact then make it visible when the player interacts with the pedestal/display case
     //We will check for the id of the artefact. If it is the second pedestal, then set the ID to 1 and we check for the second artefact
-    private void OnCollisionEnter(Collision other) 
+    private void OnTriggerEnter(Collider other) 
     {
-        if(replace_artefact && other.gameObject.tag == "Player" && GameManager.GetArtefactCollected(artefact_id))
+        if(other.gameObject.tag == "Player" && GameManager.GetArtefactCollected(artefact_id))
         {
-            Debug.Log("Placing artefact");
-            Destroy(empty_pedestal);
-            artefact.SetActive(true);
+            can_place = true;
         }
     }
 
--- Islamic_Villa_Munya/Assets/Calcifer/Script/Objects/PrefabInitiate.cs
+++ Islamic_Villa_Munya/Assets/Calcifer/Script/Objects/PrefabInitiate.cs
@@ -22,8 +22,6 @@ public class PrefabInitiate : MonoBehaviour
             //GameManager.SetArtefactCollected(false);
         }
     }
-
-
 }
 
 /*Cal's script ends here*/
