58430f6e5b4bf9d409ab23f3fbc8c15214f8d75f
DX9 <34134997+DX9T0@users.noreply.github.com>
2023-05-08 13:37:46 +0100

    added fps counter to player audio script for convenience

--- Islamic_Villa_Munya/Assets/Leon/Script/PlayerAudio.cs
+++ Islamic_Villa_Munya/Assets/Leon/Script/PlayerAudio.cs
@@ -134,4 +134,9 @@ public class PlayerAudio : MonoBehaviour
 
         setupComplete = true;
     }
+    //Leon fps counter
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 100), (1.0f / Time.smoothDeltaTime).ToString());
+    }
 }
