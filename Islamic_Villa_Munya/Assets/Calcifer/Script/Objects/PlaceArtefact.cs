652ac1864ba39ade51dc8fee804849b11009cc65
DX <leonhodgkinson123@gmail.com>
2023-05-09 14:02:06 +0100

    artefact place sound

--- Islamic_Villa_Munya/Assets/Calcifer/Script/Objects/PlaceArtefact.cs
+++ Islamic_Villa_Munya/Assets/Calcifer/Script/Objects/PlaceArtefact.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using UnityEngine.Audio;
 /*Cal's script starts here*/
 public class PlaceArtefact : MonoBehaviour
 {
@@ -20,8 +20,20 @@ public class PlaceArtefact : MonoBehaviour
     private bool can_place = false;
     private bool do_until = true;
 
+//Leon audio
+    AudioMixer mixer;
+    AudioClip placeClip;
+    AudioSource placeSound;
+
     void Start()
     {
+        mixer = Resources.Load("NewAudioMixer") as AudioMixer;
+        placeClip = Resources.Load("sfx_place_artefact 1") as AudioClip;
+        placeSound = gameObject.AddComponent<AudioSource>();
+        placeSound.clip = placeClip;
+        placeSound.outputAudioMixerGroup = mixer.FindMatchingGroups("SFX")[0];
+//leon end
+
         kb = InputSystem.GetDevice<Keyboard>();
         artefact.SetActive(true);
         if(IO != null)
@@ -47,6 +59,10 @@ public class PlaceArtefact : MonoBehaviour
         {
             if(GameManager.GetArtefactPlaced(artefact_id))
             {
+                //play leon audio
+                placeSound.Play();
+                //leon end
+
                 renderer.material = normalMat;
                 if(IO != null)
                 {
