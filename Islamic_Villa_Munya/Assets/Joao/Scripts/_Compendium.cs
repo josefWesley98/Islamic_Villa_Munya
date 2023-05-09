4f78650463614ec18de1ec6fa503b2ae125f73f5
JOÃO CRESPO GONÇALVES <2208166@uad.ac.uk>
2023-05-09 17:22:38 +0100

    Compendium configured
    Id added to compendium script that detects which is active and which is not. id 0,1,2,6,7,8,9,

--- Islamic_Villa_Munya/Assets/Joao/Scripts/_Compendium.cs
+++ Islamic_Villa_Munya/Assets/Joao/Scripts/_Compendium.cs
@@ -7,15 +7,29 @@ public class _Compendium : MonoBehaviour
     [Header("Entries_paramaters")]                          // different entries types, unknown and Discovered
     [SerializeField] private GameObject Unknown_Entry;
     [SerializeField] private GameObject Discovered_Entry;
+
+    [SerializeField] private int artefact_id;
+
+    private bool do_until = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Unknown_Entry.SetActive(true);
+        Discovered_Entry.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(do_until)
+        {
+            if(GameManager.GetArtefactCollected(artefact_id))
+            {
+                Unknown_Entry.SetActive(false);
+                Discovered_Entry.SetActive(true);
+
+                do_until = false;
+            }
+        }
     }
 }
